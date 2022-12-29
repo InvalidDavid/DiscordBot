c11f911478ab0dc04d134afb5f3db440fdae2d47
InvalidDavid <118831950+InvalidDavid@users.noreply.github.com>
2022-12-29 12:14:19 +0100

    Update minecraftstats.py
    Danke an @Guddi#9552 der den Vorschlag gemacht hat den Code von requests zu aiohttp umzucoden ^^

--- cog/minecraftstats.py
+++ cog/minecraftstats.py
@@ -1,7 +1,7 @@
-import requests
 import discord
 from discord.ext import commands
 from discord.commands import slash_command, option
+import aiohttp
 
 class mc(commands.Cog):
     def __init__(self, bot):
@@ -12,44 +12,51 @@ class mc(commands.Cog):
     async def minecraftstats(self, ctx, auswahl, ip):
         try:
             if auswahl == "Bedrock":
-                api = requests.get(f"https://api.mcsrvstat.us/bedrock/2/{ip}")
-                data = api.json()
-
-                if data["online"] == True:
-                    text = f"Online"
-                elif data["online"] == False:
-                    text = "Offline"
-                try:
-                   embed = discord.Embed(
-                    color=discord.Colour.embed_background(),
-                    title="Minecraft Server Stats",
-                    description=f"Status: {text}\nIP: {data['ip']}\nPort: {data['port']}\nVersion: {data['version']}\nSoftware: {data['software']}\nSpieler: {data['players']['online']}/{data['players']['max']}"
-             )
-                   await ctx.respond(embed=embed)
-                except:
-                    embed = discord.Embed(
-                        color=discord.Colour.embed_background(),
-                        title="Minecraft Server Stats",
-                        description=f"Status: {text}\nIP: {data['ip']}\nPort: {data['port']}"
-                    )
-                    await ctx.respond(embed=embed)
-
-
+                async with aiohttp.ClientSession() as cs:
+                    async with cs.get(f"https://api.mcsrvstat.us/bedrock/2/{ip}") as r:
+                        res = await r.json()
+                        if res['online'] == True:
+                            embed = discord.Embed(
+                                title="Minecraft Server Stats",
+                                description=f"IP: {ip} | Port: {res['port']}",
+                                color=discord.Color.embed_background()
+                            )
+                            embed.add_field(name="Online", value='Ja')
+                            embed.add_field(name="Version", value=res['version'])
+                            embed.add_field(name="Players", value=res['players']['online'])
+                            embed.add_field(name="Max Players", value=res['players']['max'])
+                            embed.add_field(name="Software", value=res['software'])
+                            await ctx.respond(embed=embed)
+                        else:
+                            embed = discord.Embed(
+                                title="Fehler",
+                                description="Der Server ist Offline",
+                                color=discord.Color.embed_background()
+                            )
+                            await ctx.respond(embed=embed)
             elif auswahl == "Java":
-                api = requests.get(f"https://api.mcsrvstat.us/2/{ip}")
-                data = api.json()
-
-                if data["online"] == True:
-                    text = f"Online"
-                elif data["online"] == False:
-                    text = "Offline"
-
-                embed = discord.Embed(
-                    color=discord.Colour.embed_background(),
-                    title="Minecraft Server Stats",
-                  description=f"Status: {text}\nIP: {data['ip']}\nPort: {data['port']}\nVersion: {data['version']}\nSoftware: {data['software']}\nSpieler: {data['players']['online']}/{data['players']['max']}"
-             )
-                await ctx.respond(embed=embed)
+                async with aiohttp.ClientSession() as cs:
+                    async with cs.get(f"https://api.mcsrvstat.us/2/{ip}") as r:
+                        res = await r.json()
+                        if res['online'] == True:
+                            embed = discord.Embed(
+                                title="Minecraft Server Stats",
+                                description=f"IP: {ip} | Port: {res['port']}",
+                                color=discord.Color.embed_background()
+                            )
+                            embed.add_field(name="Online", value='Ja')
+                            embed.add_field(name="Version", value=res['version'])
+                            embed.add_field(name="Players", value=res['players']['online'])
+                            embed.add_field(name="Max Players", value=res['players']['max'])
+                            embed.add_field(name="Software", value=res['software'])
+                            await ctx.respond(embed=embed)
+                        else:
+                            embed = discord.Embed(
+                                title="Fehler",
+                                description="Der Server ist Offline",
+                                color=discord.Color.embed_background()
+                            )
+                            await ctx.respond(embed=embed)
         except:
             await ctx.respond("Server nicht gefunden", ephemeral=True)
 
