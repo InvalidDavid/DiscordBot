ddfcc9cd9b83fc295e7219bcc80f93e99f12b835
InvalidDavid <118831950+InvalidDavid@users.noreply.github.com>
2025-07-29 16:23:34 +0200

    Update main.py
    sync command addet

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import discord
+from discord.ext import commands
 import os
 from dotenv import load_dotenv
+import datetime
 
 load_dotenv()
 
 
 bot = discord.Bot(
     intents=discord.Intents.all(),
-    debug_guilds=[int(guild) for guild in os.getenv("DEBUG_GUILDS", "").split(",") if guild.strip()]
+    debug_guilds=[int(guild) for guild in os.getenv("DEBUG_GUILDS", "").split(",") if guild.strip()],
+    sync_commands=True
 )
-
 
 
 @bot.event
 async def on_ready():
     await bot.wait_until_ready()
+
     infos = [
         f"Pycord:    {discord.__version__}",
         f"Ping:      {round(bot.latency * 1000):,}ms",
@@ -33,8 +36,17 @@ async def on_ready():
     start_txt += f"╚{(len(longest) + 2) * '═'}╝"
     print(start_txt)
 
-    # activity = discord.Game(name="mit den Chaos")
-    # await bot.change_presence(status=discord.Status.online, activity=activity)
+    activity = discord.Game(name="mit den Chaos")
+    await bot.change_presence(status=discord.Status.online, activity=activity)
+
+@bot.slash_command()
+@commands.is_owner()
+@commands.cooldown(1, 10, commands.BucketType.user)
+async def synchronisieren(ctx):
+    await bot.sync_commands(force=True)
+    print(f"{datetime.datetime.now()}: Synchronisiert von {ctx.author} ({ctx.author.id})")
+    await ctx.respond("Commands synced!", ephemeral=True)
+
 
 
 if __name__ == "__main__":
